f1fca3f1eaa5b1042de77b9028b72ea26ac2ad1b
irakozej <jeaniakoze68@gmail.com>
2023-11-03 21:16:36 +0000

    documented

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,25 +14,27 @@ if __name__ == "__main__":
 
         # Fetch user data
         user_response = requests.get(
-      "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-           )
+            "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+        )
         user_data = user_response.json()
         employee_name = user_data.get("name")
 
         # Fetch user's TODO list
         todo_response = requests.get(
-     "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
-                )
+            "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
+        )
         todo_data = todo_response.json()
 
         # Calculate TODO progress
         total_tasks = len(todo_data)
-     completed_tasks = sum(1 for task in todo_data if task.get("completed"))
+        completed_tasks = sum(1 for task in todo_data if task.get("completed"))
 
         # Print the progress
         print(
-        "Employee {} is done with tasks({}/{}):".format
-         (employee_name, completed_tasks, total_tasks))
+            "Employee {} is done with tasks({}/{}):".format(
+                employee_name, completed_tasks, total_tasks
+            )
+        )
         for task in todo_data:
             if task.get("completed"):
                 print("\t {}".format(task.get("title")))
